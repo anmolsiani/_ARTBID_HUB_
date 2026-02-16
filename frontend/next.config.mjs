64f6e97c4568c07ceef1f3d771e897a8a91c0aae
anmolsiani <ish.kumar.personal@gmail.com>
2026-02-16 22:38:27 +0530

    boom

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8dacf84a870065f6c6cfef335d1a9179cf4bb0a0f9488da4c24b9a7335102b9b
-size 474
+oid sha256:f1cba1724b212753d7b87e99601c91c833e835e163d882c9f32087d1fea2a873
+size 743
